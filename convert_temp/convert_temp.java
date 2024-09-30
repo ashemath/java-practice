f1168a09b93eaa1a526962b8aa58828a34be5300
Bill <ashemath@gmail.com>
2024-09-29 20:17:52 -0700

    parsing two styles of input

--- convert_temp/convert_temp.java
+++ convert_temp/convert_temp.java
@@ -16,6 +16,22 @@ public class convert_temp{
 
     }
 
+    public static int argsc( String[] args ){
+      int c = args.length;
+      return c;
+    }
+
+    public static String parse_degrees( String arg){
+      String degrees;
+      degrees = arg.substring(0,arg.length()-1);
+      return degrees;
+    }
+
+    public static char parse_origin( String arg){
+        char origin = arg.charAt(arg.length()-1);
+        return origin;
+    }
+
     // examples: 
     // convert_temp 32 F
     // 0 C
@@ -24,25 +40,35 @@ public class convert_temp{
     // 212 F
 
     public static void main(String[] args) {
-      String origin_unit;
-      double degrees = Double.parseDouble(args[0]);
-      origin_unit = args[1];
-
+      char origin_unit;
+      double degrees;
+      if (args.length == 1){
+          degrees = Double.parseDouble(parse_degrees(args[0])); 
+          origin_unit = parse_origin(args[0]);
+      }
+      else if (args.length == 2){
+        degrees = Double.parseDouble(args[0]);
+        origin_unit = args[1].charAt(0);
+      } else {
+        origin_unit = 'z';
+        degrees = 42;
+      }
       switch (origin_unit) {
-        case "C":
+        case 'C':
             System.out.println(c2F(degrees)+"F");
             break;
-        case "c":
+        case 'c':
             System.out.println(c2F(degrees)+"F");
             break;
-        case "F":
+        case 'F':
             System.out.println(f2C(degrees)+"C");
             break;
-        case "f":
+        case 'f':
             System.out.println(f2C(degrees)+"C");
             break;
         default:
             System.out.println("error, see example below:\n convert_temp 32 f");
+            //System.out.println(parse_degrees("310f") + parse_origin("610x"));
 
       }
 
