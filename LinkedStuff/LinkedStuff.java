6b7dd998a89ad53ff7405fe94fcd1867dd18fefa
Bill <ashemath@gmail.com>
2024-11-13 22:03:06 -0800

    Added more methods. Working prepend, insert, and copy.

--- LinkedStuff/LinkedStuff.java
+++ LinkedStuff/LinkedStuff.java
@@ -2,8 +2,8 @@
  * LinkedStuff is a generic singly-linked list data structure
  */
 public class LinkedStuff<T> {
-    static int length = 1;
-  public Node<T> head;
+    public Integer length = 1;
+    public Node<T> head;
 
     static class Node<T>{
         T data;
@@ -22,6 +22,78 @@ public class LinkedStuff<T> {
     public LinkedStuff(T d){
         head = new Node<T>(d);
     }
+
+    public LinkedStuff<T> copy() {
+        return this.first(this.length);
+    }
+
+    /**
+     * step
+     * return a sub-list with the first n linked data nodes.
+     * @param int   n   The number of levels to decend through the list.
+     */
+    public LinkedStuff<T> first(int n) {
+        if ( n <= 0 ){
+            return null;
+        }
+        LinkedStuff<T> sub = new LinkedStuff<T>(head.data);
+        int new_length = 1;
+        sub.length = new_length;
+        Node<T> index = this.head;
+        while( n > 1 ){
+            index = index.next;
+            sub.append(index.data);
+            new_length++;
+            n--;
+        }
+        sub.length = new_length;
+        return sub;
+    }
+
+    /**
+     * head
+     * recall the data stored at the head of the list
+     * @param   none
+     */
+    public T head(){
+        T item = null;
+        if (head.data != null){
+            item = head.data;
+        }
+        return item;
+    }
+
+    public void insert(int n, T d){
+        Node<T> new_node = new Node(d);
+        Node<T> index = this.head;
+        Node<T> before = null;
+        Node<T> after = null;
+        if ( n==0 ){
+            prepend(d);
+        } else {
+            while ( n != 0 && index.next != null ){
+                before = index;
+                after = index.next;
+                index = index.next;
+                n--;
+            }
+            before.next = new_node;
+            new_node.next = after;
+            length++;
+        }
+    }
+
+    /**
+     * Prepend
+     * Add a new string to the beginning of the list.
+     */
+    public void prepend(T d){
+         Node<T> new_node = new Node<T>(d);
+         Node<T> after = head;
+         head = new_node;
+         head.next = after;
+         length++;
+     }
 
     /**
      * append:
@@ -52,7 +124,7 @@ public class LinkedStuff<T> {
 
         Node<T> readHead = this.head;
         while ( readHead !=  null ){
-            buffer.append( readHead.data.toString() );
+            buffer.append( readHead.data.toString());
             readHead = readHead.next;
             if (readHead != null ){
                 buffer.append(" ");
@@ -65,9 +137,24 @@ public class LinkedStuff<T> {
         LinkedStuff<Double> list = new LinkedStuff<Double>(0.5);
         list.append(0.4);
         list.append(2.45);
+        list.prepend(3.14);
+
+        LinkedStuff<Double> list2 = list.first(4);
         String output = list.readStuff();
+        System.out.println("the data at head is: " + list.head().toString());
         System.out.println(output);
-        System.out.println("Length is: " + length);
+        System.out.println("Length is: " + list.length);
+        String output2 = list2.readStuff();
+        System.out.println(output2);
+        System.out.println("Length of list2 is: " + list2.length );
+
+        LinkedStuff<Double> list3 = list2.copy();
+        list3.insert(4, 6.66);
+        list3.insert(3, 42.66);
+        list3.insert(0, 0.42);
+        String output3 = list3.readStuff();
+        System.out.println(output3);
+        System.out.println("Length of list3 is: " + list3.length );
     }
 
 }
